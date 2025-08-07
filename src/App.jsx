c532a5cc0d726b744b721b5447df01864b1de9f4
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-08-07 21:21:13 +0300

    Create Header and Login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <>
-    Form project 
+    <Header />
+    <main>
+      <Login />
+    </main>
     </>
   )
 }
