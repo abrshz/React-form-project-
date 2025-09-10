377909a5f7563bb9ca7636ab60e15e5c7bf21e2b
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-09-10 23:49:33 +0300

    Changed the Form  onSumbit attribute to action

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -21,7 +21,7 @@ function SignUp() {
         event.target.reset();
     }
   return (  
-    <form onSubmit={handleSubmit}>
+    <form action={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
