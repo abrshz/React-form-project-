0540c541d0c0da0fb783f4b1266bcd86527b3f37
Abraham Haile Araya <107077258+abrshz@users.noreply.github.com>
2025-09-12 05:14:00 +0300

    Moving the action function out the components

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -49,10 +49,19 @@ function SignUp() {
         }
 
         if(errors.length > 0) {
-          return{errors}
+          return{errors , enteredValues: {
+            email,
+            password,
+            confirmPassword ,
+            firstName,
+            lastName,
+            role,
+            acquisitionChannel,
+            terms
+          }}
         }
 
-        return {errors: null}
+        return {errors: null }
 
 
         // const fd = new FormData(event.target);
@@ -76,13 +85,13 @@ function SignUp() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" required />
+        <input id="email" type="email" name="email" defaultValue={formState?.enteredValues?.email} />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" required minLength={6} />
+          <input id="password" type="password" name="password" minLength={6} defaultValue={formState?.enteredValues?.password} />
         </div>
 
         <div className="control">
@@ -91,7 +100,7 @@ function SignUp() {
             id="confirm-password"
             type="password"
             name="confirm-password"
-            required
+            defaultValue={formState?.enteredValues?.confirmPassword}
           />
           {/* <div className="control-error">{passwordNotSame && <p>Passwords must match!</p>}</div> */}
         </div>
@@ -102,18 +111,18 @@ function SignUp() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" required />
+          <input type="text" id="first-name" name="first-name" defaultValue={formState?.enteredValues?.firstName} />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" required />
+          <input type="text" id="last-name" name="last-name" defaultValue={formState?.enteredValues?.lastName} />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role" required >
+        <select id="role" name="role" defaultValue={formState?.enteredValues?.role} >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -130,6 +139,7 @@ function SignUp() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -140,19 +150,20 @@ function SignUp() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input type="checkbox" id="other" name="acquisition" value="other" defaultChecked={formState.enteredValues?.acquisitionChannel.includes('other')} />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" required  />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" defaultChecked={formState.enteredValues?.terms} />I
           agree to the terms and conditions
         </label>
       </div>
